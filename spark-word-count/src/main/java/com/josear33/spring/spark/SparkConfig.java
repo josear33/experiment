b601822c38b1f24acd307f58a71654842074e87a
Josear33 <ja.rodjimenez@gmail.com>
2020-04-03 20:12:35 +0200

    Springboot + REST wrapper for Spark

---
+++ spark-word-count/src/main/java/com/josear33/spring/spark/SparkConfig.java
@@ -0,0 +1,43 @@
+package com.josear33.spring.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SparkConfig {
+	@Value("${spark.app.name}")
+	private String appName;
+	@Value("${spark.master}")
+	private String masterUri;
+
+	@Bean
+	public SparkConf conf() {
+		return new SparkConf().setAppName(appName).setMaster(masterUri);
+	}
+
+	@Bean
+	public JavaSparkContext sc() {
+		return new JavaSparkContext(conf());
+	}
+
+	public String getAppName() {
+		return appName;
+	}
+
+	public SparkConfig setAppName(String appName) {
+		this.appName = appName;
+		return this;
+	}
+
+	public String getMasterUri() {
+		return masterUri;
+	}
+
+	public SparkConfig setMasterUri(String masterUri) {
+		this.masterUri = masterUri;
+		return this;
+	}
+}
---
+++ spark-word-count/src/main/java/com/josear33/spring/spark/SparkWordCountApplication.java
@@ -0,0 +1,13 @@
+package com.josear33.spring.spark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkWordCountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkWordCountApplication.class, args);
+	}
+
+}
